43abc5fdd5c7a9ba36efab73b32184f968b7f12b
Aagman Suri <aagmansuri96@gmail.com>
2022-02-04 11:02:39 +0530

    media querry added for hamburger

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
 const hide = document.getElementById("hide");
 const btn = document.getElementById("btn");
 
-let isHide = false;
+// let isHide = true;
+// const hideFunction = () => {
+//   isHide = !isHide;
+//   isHide ? (hide.style.display = "flex") : (hide.style.display = "none");
+// };
+hide.style.display = "none";
 const hideFunction = () => {
-  isHide = !isHide;
-  isHide ? (hide.style.display = "none") : (hide.style.display = "block");
+  if (hide.style.display === "flex") {
+    hide.style.display = "none";
+  } else {
+    hide.style.display = "flex";
+  }
 };
 
 btn.addEventListener("click", hideFunction);
