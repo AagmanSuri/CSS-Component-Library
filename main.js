68e41814028b49bb95097f936ef1666781dbb42d
Aagman Suri <aagmansuri96@gmail.com>
2022-02-04 11:54:15 +0530

    icon update on btn click

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const hide = document.getElementById("hide");
 const btn = document.getElementById("btn");
 
+const img = document.getElementById("ham-icon-id");
+
 // let isHide = true;
 // const hideFunction = () => {
 //   isHide = !isHide;
@@ -10,8 +12,10 @@ hide.style.display = "none";
 const hideFunction = () => {
   if (hide.style.display === "flex") {
     hide.style.display = "none";
+    img.src = "https://api.iconify.design/cil:hamburger-menu.svg";
   } else {
     hide.style.display = "flex";
+    img.src = "https://api.iconify.design/maki:cross-11.svg";
   }
 };
 
